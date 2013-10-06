59cd069219e05b8a64d47a03752cbf2ec956fb6d
Bassam Tabbara <bassam@symform.com>
2013-10-06 15:53:16 -0700

    Experimental support for threading and FIPS model.

--- class/Crimson.OpenSsl/Crimson.OpenSsl/Native.cs
+++ class/Crimson.OpenSsl/Crimson.OpenSsl/Native.cs
@@ -23,19 +23,61 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
+// #define OPENSSL_THREADING
+// #define OPENSSL_FIPS
+
 namespace Crimson.OpenSsl
 {
 	using Microsoft.Win32.SafeHandles;
 	using System;
+	using System.Threading;
 	using System.Runtime.InteropServices;
 	using System.Security.Cryptography;
+	using System.Collections.Generic;
 
 	/// <summary>
 	/// Documentation can be found here: http://www.openssl.org/docs/crypto/EVP_DigestInit.html
 	/// </summary>
 	internal static class Native
 	{
-		const string DllName = "libcrypto";
+		private const string DllName = "libcrypto";
+
+#if OPENSSL_THREADING
+		private static object initLock = new object ();
+		private static List<object> locks;
+		private static Native.CRYPTO_locking_callback lockingDelegate;
+		private static Native.CRYPTO_id_callback threadDelegate;
+		private static HashSet<uint> threads;
+
+		static Native ()
+		{
+			lock (initLock) {
+				if (!IsInitialized ()) {
+#if OPENSSL_FIPS
+					try {
+						FIPS_mode_set (1);
+						Console.WriteLine ("FIPS mode set to 1");
+					}
+					catch (EntryPointNotFoundException) {
+						Console.WriteLine ("FIPS not found.");
+					}
+#endif
+					InitializeThreads ();
+					AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+				}
+			}
+		}
+
+		private static void OnProcessExit (object sender, EventArgs args)
+		{
+			lock (initLock) {
+				if (IsInitialized ()) {
+					AppDomain.CurrentDomain.ProcessExit -= OnProcessExit;
+					UninitializeThreads ();
+				}
+			}
+		}
+#endif
 
 		public static void ExpectSuccess (bool ret)
 		{
@@ -43,9 +85,8 @@ namespace Crimson.OpenSsl
 				throw new CryptographicException ();
 			}
 		}
-		//
-		// Version
-		//
+
+		#region versioninfo
 		[Serializable]
 		public enum SSLeayVersionType
 		{
@@ -58,9 +99,105 @@ namespace Crimson.OpenSsl
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false, CharSet = CharSet.Ansi)]
 		public extern static IntPtr SSLeay_version (SSLeayVersionType type);
-		//
-		// Digests
-		//
+		#endregion
+
+#if OPENSSL_FIPS
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static int FIPS_mode_set(int onoff);
+#endif
+
+#if OPENSSL_THREADING
+		#region threading
+		public const int CRYPTO_LOCK = 1;
+
+		[UnmanagedFunctionPointer (CallingConvention.Cdecl)]
+		public delegate void CRYPTO_locking_callback (int mode, int type, string file, int line);
+
+		[UnmanagedFunctionPointer (CallingConvention.Cdecl)]
+		public delegate uint CRYPTO_id_callback ();
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static void CRYPTO_set_id_callback (CRYPTO_id_callback cb);
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static IntPtr CRYPTO_get_id_callback ();
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static void CRYPTO_set_locking_callback (CRYPTO_locking_callback cb);
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static int CRYPTO_num_locks ();
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static void ERR_remove_state (uint pid);
+
+		[DllImport(DllName, CallingConvention=CallingConvention.Cdecl)]
+		public extern static void ERR_clear_error ();
+
+		private static bool IsInitialized ()
+		{
+			return Native.CRYPTO_get_id_callback () != IntPtr.Zero;
+		}
+
+		private static void InitializeThreads ()
+		{
+			int lockCount = Native.CRYPTO_num_locks ();
+
+			locks = new List<object> (lockCount);
+			for (int i = 0; i < lockCount; i++) {
+				locks.Add (new object ());
+			}
+
+			threads = new HashSet<uint> ();
+
+			lockingDelegate = LockingCallback;
+			Native.CRYPTO_set_locking_callback (lockingDelegate);
+
+			threadDelegate = ThreadCallback;
+			Native.CRYPTO_set_id_callback (threadDelegate);
+		}
+
+		private static void UninitializeThreads ()
+		{
+			// Cleanup the thread lock objects
+			Native.CRYPTO_set_locking_callback (null);
+			lockingDelegate = null;
+
+			Native.CRYPTO_set_id_callback (null);
+			threadDelegate = null;
+
+			if (threads != null) {
+				foreach (uint id in threads) {
+					Native.ERR_clear_error ();
+					Native.ERR_remove_state (id);
+				}
+				threads.Clear ();
+			}
+		}
+
+		public static void LockingCallback (int mode, int type, string file, int line)
+		{
+			if ((mode & Native.CRYPTO_LOCK) == Native.CRYPTO_LOCK) {
+				Monitor.Enter (locks [type]);
+			} else {
+				Monitor.Exit (locks [type]);
+			}
+		}
+
+		public static uint ThreadCallback ()
+		{
+			uint thread = (uint)Thread.CurrentThread.ManagedThreadId;
+
+			lock (threads) {
+				threads.Add (thread);
+			}
+
+			return thread;
+		}
+		#endregion
+#endif
+
+		#region digest
 		public const int MaximumDigestSize = 64;
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
@@ -73,22 +210,28 @@ namespace Crimson.OpenSsl
 		public extern static SafeDigestHandle EVP_sha256 ();
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
-		public extern static SafeDigestContextHandle EVP_MD_CTX_create ();
+		public extern static IntPtr EVP_MD_CTX_create ();
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
-		private extern static void EVP_MD_CTX_destroy (IntPtr ctx);
+		public extern static IntPtr EVP_MD_CTX_init (IntPtr ctx);
+
+		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+		public extern static void EVP_MD_CTX_cleanup (IntPtr ctx);
+
+		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+		public extern static void EVP_MD_CTX_destroy (IntPtr ctx);
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
 		[return: MarshalAs(UnmanagedType.Bool)]
-		public extern static bool EVP_DigestInit_ex (SafeDigestContextHandle ctx, SafeDigestHandle type, IntPtr impl);
+		public extern static bool EVP_DigestInit_ex (IntPtr ctx, SafeDigestHandle type, IntPtr impl);
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
 		[return: MarshalAs(UnmanagedType.Bool)]
-		public extern static bool EVP_DigestUpdate (SafeDigestContextHandle ctx, IntPtr d, uint cnt);
+		public extern static bool EVP_DigestUpdate (IntPtr ctx, IntPtr d, uint cnt);
 
 		[DllImport (DllName, CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
 		[return: MarshalAs(UnmanagedType.Bool)]
-		public extern static bool EVP_DigestFinal_ex (SafeDigestContextHandle ctx, IntPtr md, out uint s);
+		public extern static bool EVP_DigestFinal_ex (IntPtr ctx, IntPtr md, out uint s);
 
 		internal sealed class SafeDigestHandle : SafeHandleZeroOrMinusOneIsInvalid
 		{
@@ -122,9 +265,9 @@ namespace Crimson.OpenSsl
 				return true;
 			}
 		}
-		//
-		// Ciphers
-		//
+		#endregion
+
+		#region ciphers
 		[Serializable]
 		public enum CipherOperation
 		{
@@ -205,5 +348,6 @@ namespace Crimson.OpenSsl
 				return true;
 			}
 		}
+		#endregion
 	}
 }
